2294de99daac9b2754136854697089a95ee4807e
0megq <nvatanshenas55@gmail.com>
2024-02-04 13:15:29 -0800

    Setup a basic ECS
    Untested at the moment. I need to add some sort of way of testing this. idk how though yet. goodbye for now

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include "raylib.h"
+#include "raymath.h"
+#include "main.h"
+#include <stddef.h>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -10,10 +13,13 @@ int main(void)
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+	InitWindow(screenWidth, screenHeight, "Trials of Yarbil");
 
 	SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 	//--------------------------------------------------------------------------------------
+
+	EntityComponentList components;
+	InitializeEntityComponentList(&components);
 
 	// Main game loop
 	while (!WindowShouldClose()) // Detect window close button or ESC key
@@ -41,4 +47,61 @@ int main(void)
 	//--------------------------------------------------------------------------------------
 
 	return 0;
+}
+
+void InitializeEntityComponentList(EntityComponentList *components)
+{
+	// Initialize Single Components
+	components->directionalInputComponent.direction = Vector2Zero();
+
+	// Initialize Components
+	for (int i = 0; i < MAX_ENTITIES; i++)
+	{
+		components->colliderComponents[i].entityId = NULL_ENTITY_ID;
+		components->positionComponents[i].entityId = NULL_ENTITY_ID;
+		components->velocityComponents[i].entityId = NULL_ENTITY_ID;
+		components->flagsComponents[i].entityId = NULL_ENTITY_ID;
+	}
+
+	// Total active entities and buffer size should be set to 0
+	components->totalActiveEntities = 0;
+	components->idBufferSize = 0;
+}
+
+// Creates a new entity in the given EntityComponentList components and returns the id of that entity in the list
+int NewEntity(EntityComponentList *components)
+{
+	// Get the new entity's id. If the id buffer is larger than zero, then use the id at the top of the buffer stack
+	// and decrease the buffer size,
+	// otherwise use the amount of total active entities
+	int entityId = components->idBufferSize > 0 ? components->idBuffer[components->idBufferSize--] : components->totalActiveEntities;
+
+	// Increase the total active entities whether or not we get the id from the buffer or not.
+	components->totalActiveEntities++;
+	// Set the entity with entityId to be active
+	components->entityIsActive[entityId] = true;
+	// Give the entity a flags component
+	components->flagsComponents[entityId].entityId = entityId;
+
+	return entityId;
+}
+
+// Frees the entity at the given
+void FreeEntity(EntityComponentList *components, int entityId)
+{
+	// If the entity is not active then we don't need to free it again
+	if (!components->entityIsActive[entityId])
+		return;
+	// Set the entityId of all the components of this entity to null entity id
+	components->colliderComponents[entityId].entityId = NULL_ENTITY_ID;
+	components->positionComponents[entityId].entityId = NULL_ENTITY_ID;
+	components->velocityComponents[entityId].entityId = NULL_ENTITY_ID;
+	components->flagsComponents[entityId].entityId = NULL_ENTITY_ID;
+
+	// Set the entity to not be active
+	components->entityIsActive[entityId] = false;
+	// Decrease the amount of active entities
+	components->totalActiveEntities--;
+	// Add the entity id to the id buffer
+	components->idBuffer[components->idBufferSize++] = entityId;
 }
---
+++ src/main.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "raylib.h"
+
+#define NULL_ENTITY_ID -1
+#define MAX_ENTITIES 100
+
+enum ShapeType
+{
+	TYPE_CIRCLE,
+	TYPE_RECTANGLE
+};
+
+union Shape
+{
+	Vector2 size;
+	float radius;
+};
+
+// Position Component
+struct Position
+{
+	int entityId;
+	Vector2 pos;
+};
+
+// Collider Component
+struct Collider
+{
+	int entityId;
+	enum ShapeType type;
+	union Shape *shape;
+};
+
+// Velocity Component
+struct Velocity
+{
+	int entityId;
+	Vector2 vel;
+};
+
+// There only needs to be one directional input component at a single time
+struct DirectionalInput
+{
+	Vector2 direction;
+};
+
+// Every entity will have a flags component
+struct Flags
+{
+	int entityId;
+	unsigned player_input : 1;
+};
+
+typedef struct EntityComponentList
+{
+	// Single Components i.e. components that are universal across entities like input
+	struct DirectionalInput directionalInputComponent;
+
+	// Components
+	struct Collider colliderComponents[MAX_ENTITIES];
+	struct Position positionComponents[MAX_ENTITIES];
+	struct Velocity velocityComponents[MAX_ENTITIES];
+	struct Flags flagsComponents[MAX_ENTITIES];
+
+	// Other data
+	bool entityIsActive[MAX_ENTITIES];
+	int totalActiveEntities;
+	int idBuffer[MAX_ENTITIES];
+	int idBufferSize;
+} EntityComponentList;
